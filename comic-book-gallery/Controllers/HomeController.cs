f0c57f5993318b9b6ff6e71f566d0729b08d76cd
patrikMedur01 <84334238+patrikMedur01@users.noreply.github.com>
2023-04-20 18:05:30 +0200

    Initial commit

--- comic-book-gallery/Controllers/HomeController.cs
+++ comic-book-gallery/Controllers/HomeController.cs
@@ -23,6 +23,23 @@ namespace comic_book_gallery.Controllers
             return View();
         }
 
+        public IActionResult Detail()
+        {
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
